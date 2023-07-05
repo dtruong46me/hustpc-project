c7e9c3d6be2c5c3a896288a4f270b3ca4b4e468a
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2023-07-05 10:06:17 +0700

    create database and insert sample data

---
+++ database/insert_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO categories VALUES 
+    ('category1', 'CPU', 'The component of a computer system that controls the interpretation and execution of instructions. The CPU of a PC consists of a single microprocessor, while the CPU of a more powerful mainframe consists of multiple processing devices, and in some cases, hundreds of them. The term “processor” is often used to refer to a CPU.'),
+    ('category2', 'RAM', 'A small printed circuit board that plugs into a socket on a personal computer and increases the available random-access memory (RAM).'),
+    ('category3', 'CPU Cooler', 'CPU coolers are designed to dissipate heat produced by the processor that sits at the heart of your PC. The fans, radiators, and other elements in these cooling components allow for the accumulated heat energy to flow away from vital working parts without damaging the hardware.');
+
+INSERT INTO products VALUES 
+    ('CPU-001', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 50),
+    ('CPU-002', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 60),
+    ('CPU-003', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 80),
+    ('CPU-004', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 200),
+    ('CPU-005', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 100),
+    ('CPU-006', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 150),
+    ('CPU-007', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 55),
+    ('CPU-008', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 65),
+    ('CPU-009', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 142),
+    ('CPU-010', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 62),
+    ('CPU-011', 'AMD Ryzen 9 5900X Desktop Processor', 'Brand: AMD, Processors Type: Desktop, Series:Ryzen 9 5000 Series, Name:Ryzen 9 5900X, Model:100-100000061WOF', 349.95, 'category1', 84);
