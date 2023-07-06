738f0271327785a92410ad4af2303f40b8781bde
Dinh Nguyen Cong Quy <congquyk66hust@gmail.com>
2023-07-07 00:06:29 +0700

    Complete Operating System database

--- database/insert_operating_system.sql
+++ database/insert_operating_system.sql
@@ -1,5 +1,102 @@
 INSERT INTO categories (category_id, category_name) VALUES 
+    ('category1', 'CPU'),
+    ('category2', 'RAM'),
+    ('category3', 'CPU Cooler'),
+    ('category4', 'Motherboard'),
+    ('category5', 'Video Card'),
+    ('category6', 'Case'),
+    ('category7', 'Power Supplies'),
+    ('category8', 'Storages'),
+    ('category9', 'Operating System'),
+    ('category10', 'Accessories');
+
 
 INSERT INTO products (product_id, pname, description, category_id, qty_in_store) VALUES
+    ('OS-001', 'Microsoft Windows 11 Home (Digital Download)','Less chaos, more calm. The refreshed design of Windows 11 enables you to do what you want effortlessly.
+Biometric logins. Encrypted authentication. And, of course, advanced antivirus defenses. Everything you need, plus more, to protect you against the latest cyberthreats.
+Make the most of your screen space with snap layouts, desktops, and seamless redocking.
+Widgets makes staying up-to-date with the content you love and the news you care about, simple.
+Stay in touch with friends and family with Microsoft Teams, which can be seamlessly integrated into your taskbar.
+Play the latest games with graphics that rival reality. DirectX 12 Ultimate comes ready to maximize your hardware.','category9',205),
 
-INSERT INTO configurations(product_id, config_name, config_price) VALUES
+     ('OS-002', 'Microsoft Windows 10 Enterprise LTSC 2021 Upgrade | Modern Software in CSP | Perpetual | Tenant ID Required | Commercial Business End User', 'Email us at licensing@neweggbusiness.com for a quote
+Requires a tenant ID - not sure? We can help!
+Commercial Business End User', 'category9', 169),
+
+     ('OS-003', 'Microsoft Windows 11 Home 64-bit (Product Key Code Email Delivery) - OEM', 'Windows 11 has easy-to-use tools that can help you optimize your screen space and maximize your productivity
+With Chat from Microsoft Teams you can reach anyone (for free) however you prefer right from your desktop
+The apps you need work seamlessly with new multi-tasking tools like Snap layouts, Desktops, and a new more intuitive redocking experience
+Play the latest games with graphics that rival reality. Play with players on console. Play with the peripherals that you love. When it comes to playing your way, Windows 11 makes it happen
+Windows OEM is intended for pre-installation on a new PC and cannot be transferred to another computer once installed.
+Installation is only possible when using a clean (blank) machine with no other prior version of Windows already installed.', 'category9', 138),  	
+
+     ('OS-004', 'Microsoft Windows 10 Home, 64-bit, DVD - OEM', 'The Start menu is back and better than ever so you can expand and customize it.
+Fast and responsive. Technologies like InstantGo lets you boot up and resume quickly; Video Game Platform: PC
+Windows 10 comes with apps that work across your devices - Photos, Maps, Music Video and more.
+Windows 10 OEM is a full version of the operating system not an upgrade.
+The OEM operating system is not supported by Microsoft. To acquire Windows software with support provided by Microsoft please see our full package "Retail" product.
+Windows 10 OEM is intended for pre-installation on a new PC and cannot be transferred to another computer once installed. Product ships in a white envelope.
+The Windows 11 upgrade will be delivered to qualifying devices. Timing varies by device. Certain features require additional hardware', 'category9', 150),
+
+     ('OS-005', 'Microsoft Windows 11 Pro Upgrade [from Home to Pro] [Digital Download]', 'Upgrade Windows 11 Home Device to Windows 11 Pro (Check System Requirements)
+Upgrade Windows 10 Home Device to Windows 10 Pro (Check System Requirements)
+Microsoft Windows 11 Pro Upgrade
+Check ABS/Helix computers with Windows 11
+Simpler, more intuitive UI and effortless navigation. New features like snap layouts help you manage multiple tasks with ease.
+Have effective online meetings. Share content and mute/unmute right from the taskbar.Stay focused with intelligent noise cancelling and background blur.
+Have confidence that your applications will work. Familiar deployment and update tools. Accelerate adoption with expanded deployment policies.
+Safeguard data and access anywhere with hardware-based isolation, encryption, and malware protection built in.', 'category9', 124),  
+
+     ('OS-006', 'Microsoft Windows 11 Pro 64-bit, DVD - OEM', 'A simple, powerful UX helps improve productivity and focus.
+New features enable you to make the most of available desktop space.
+App compatibility and cloud management make adoption easy.
+A Zero Trust-ready OS helps protect data and access, wherever business takes you.
+Windows OEM is to be installed on a new PC with no prior version of Windows installed and cannot be transferred to another machine.
+OEM does not provide support | To acquire product with Microsoft support, obtain the full packaged “Retail” version.
+Product ships in a plain envelope | Activation key is located under scratch-off area on label.', 'category9', 350),
+
+     ('OS-007', 'Microsoft Windows 11 Home (USB)', 'Less chaos, more calm. The refreshed design of Windows 11 enables you to do what you want effortlessly.
+Biometric logins. Encrypted authentication. And, of course, advanced antivirus defenses. Everything you need, plus more, to protect you against the latest cyberthreats.
+Make the most of your screen space with snap layouts, desktops, and seamless redocking.
+Widgets makes staying up-to-date with the content you love and the news you care about, simple.
+Stay in touch with friends and family with Microsoft Teams, which can be seamlessly integrated into your taskbar.
+Play the latest games with graphics that rival reality. DirectX 12 Ultimate comes ready to maximize your hardware.', 'category9', 363),
+
+     ('OS-008', 'Microsoft Windows 11 Pro (USB)', 'Instantly productive. Simpler, more intuitive UI and effortless navigation. New features like snap layouts help you manage multiple tasks with ease.
+Smarter collaboration. Have effective online meetings. Share content and mute/unmute right from the taskbar. Stay focused with intelligent noise cancelling and background blur.
+Reassuringly consistent. Have confidence that your applications will work. Familiar deployment and update tools. Accelerate adoption with expanded deployment policies.
+Powerful security. Safeguard data and access anywhere with hardware-based isolation, encryption, and malware protection built in.', 'category9', 420),
+
+     ('OS-009', 'Microsoft Windows 11 Home 64-bit, DVD - OEM', 'Less chaos, more calm. The refreshed design of Windows 11 enables you to do what you want effortlessly.
+Biometric logins.**. Encrypted authentication. And, of course, advanced antivirus defenses. Everything you need, plus more, to protect you against the latest cyberthreats.
+Make the most of your screen space with snap layouts, desktops, and seamless redocking.
+Widgets makes staying up-to-date with the content you love and the news you care about, simple.
+Play the latest games with graphics that rival reality. DirectX 12 Ultimate comes ready to maximize your hardware.**
+Windows OEM is to be installed on a new PC with no prior version of Windows installed and cannot be transferred to another machine.
+OEM does not provide support | To acquire product with Microsoft support, obtain the full packaged “Retail” version.
+Product ships in a plain envelope | Activation key is located under scratch-off area on label.', 'category9', 69),
+
+     ('OS-010', 'Microsoft Windows 11 Pro (Digital Download)', 'Instantly productive. Simpler, more intuitive UI and effortless navigation. New features like snap layouts help you manage multiple tasks with ease.
+Smarter collaboration. Have effective online meetings. Share content and mute/unmute right from the taskbar. Stay focused with intelligent noise cancelling and background blur.
+Reassuringly consistent. Have confidence that your applications will work. Familiar deployment and update tools. Accelerate adoption with expanded deployment policies.
+Powerful security. Safeguard data and access anywhere with hardware-based isolation, encryption, and malware protection built in.', 'category9', 289);
+
+INSERT INTO configurations (product_id, config_name, config_price) VALUES
+     ('OS-001','Home', 129.99),
+     ('OS-001','Pro', 189.99),
+     ('OS-002','Home', 295.00),
+     ('OS-003','Home', 129.99),
+     ('OS-003','Pro', 199.99),
+     ('OS-004','Home', 129.99),
+     ('OS-005','Pro Upgrade', 99.00),
+     ('OS-005','Pro', 189.99),
+     ('OS-006','Home', 119.98),
+     ('OS-006','Pro', 152.41),
+     ('OS-007','Home', 138.98),
+     ('OS-007','Pro', 198.99),
+     ('OS-008','Pro', 198.99),
+     ('OS-009','Home', 119.98),
+     ('OS-010','Home', 129.99),
+     ('OS-010','Pro Upgrade', 99.00),
+     ('OS-010','Pro', 189.99);
+ 
--- database/insert_storages.sql
+++ database/insert_storages.sql
@@ -25,89 +25,164 @@ INSERT INTO products (product_id, pname, description, category_id, qty_in_store)
 	'category8',
 	151),
 	('STG-003',
-	'MSI SPATIUM M450 M.2 2280 1TB PCI-Express 4.0 x4, NVMe 1.4 3D NAND Internal Solid State Drive (SSD)',
-	'SPATIUM was envisioned to expand our ecosystem and cover the high-performance storage category. Built with high-quality, high-density 3D NAND flash, SPATIUM SSDs deliver the best compromise of performance and endurance for professionals, content creators, and gamers.',
+	'WD_BLACK SN850X NVMe M.2 2280 2TB PCI-Express 4.0 x4 Internal Solid State Drive (SSD) WDS200T2X0E',
+	'Get the ultimate gaming edge over your competition with insane speeds up to 7,300 MB/s for top-level performance and ridiculously short load times.
+Soup up your gaming experience with extremely low latency that loads graphics fast with minimal stutter and lagging for smooth, satisfying action.
+A range of capacities from 1TB to 4TB means you get to keep more of today’s games that can take up 200GB or more of storage.
+The 1TB and 2TB drives have an optional version with RGB lighting plus heatsink to help maintain peak performance through the most intense gaming sessions.
+Game Mode 2.0 serves up even more PC performance-boosting features like load prediction to ready game assets for fast in-game loading.
+Output approaching the limits of the PCIe® Gen4 interface helps you get ultra-fast performance out of your storage and system components.',
 	'category8',
 	152),
 	('STG-004',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'MSI SPATIUM M450 M.2 2280 1TB PCI-Express 4.0 x4, NVMe 1.4 3D NAND Internal Solid State Drive (SSD)',
+	'PCIe Gen4x4 interface and complies with the NVMe 1.4 standard
+Sequential Read speeds up to 3600MB/s and Write speeds up to 3000MB/s
+Up to 600 TBW
+Built-in data security and error-correction capabilities
+250GB to 1TB capacities in M.2 2280 form factor
+Suitable for both desktop and notebook',
 	'category8',
 	153),
+
 	('STG-005',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Colorful SSD 1TB M.2 2280 NVMe Gen3 x 4 PCIe 3D NAND, Internal Solid State Drive Read Up to 2100 MB/s, Model CN600 1TB',
+	'[High Performance] Colorful CN600 1TB Internal Solid State Drive delivers undeniable performance, reliability and compatibility.
+[More Efficient] Sequential read speeds up to 2100MB/s and sequential write speeds up to 1700MB/s. Ultra-fast read speeds, zero wait for gaming & office loading.
+[More Durable] More durable than traditional hard drives, shock and vibration resistant, rugged and drop resistant, protects against data loss.
+[Longer Lasting] SSDs have an MTBF (Mean Time Between Failure) of 1 million hours. Low power consumption and noiseless operation effectively extend battery life.
+[High Quality] High quality GOOD DIE original chip for peace of mind that they will last.',
 	'category8',
 	154),
 		
 	('STG-006',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'CORN Professional M.2 SSD 512GB 1TB 2TB PCIe 4.0X4 solid state drive TLC For Desktop PC Laptop PS5 2TB',
+	'Interface type: M.2
+Capacity:512GB/1TB/2TB
+Sequential Read:5000MB/s/5000MB/s/5000MB/s
+Sequential Write:2500MB/s/4500MB/s/4500MB/s
+TBW:300TB/600TB/1200TB
+MTBF:1.5 million hours',
 	'category8',
 	155),
 
 	('STG-007',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Colorful SSD 2TB M.2 2280 NVMe Gen3 x 4 PCIe 3D NAND, Internal Solid State Drive Read Up to 2500 MB/s, Model CN600 2TB',
+	'[High Performance] Colorful CN600 2TB Internal Solid State Drive delivers undeniable performance, reliability and compatibility.
+[More Efficient] Sequential read speeds up to 2500MB/s and sequential write speeds up to 1700MB/s. Ultra-fast read speeds, zero wait for gaming & office loading.
+[More Durable] More durable than traditional hard drives, shock and vibration resistant, rugged and drop resistant, protects against data loss.
+[Longer Lasting] SSDs have an MTBF (Mean Time Between Failure) of 2 million hours. Low power consumption and noiseless operation effectively extend battery life.
+[High Quality] High quality GOOD DIE original chip for peace of mind that they will last.',
 	'category8',
 	156),
+
 	('STG-008',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Nextorage Japan 2TB NVMe M.2 2280 PCIe Gen.4 Internal SSD Read Speed up to 7300MB/s Write Speed Up to 6900 MB/s (New G-Series). Read Speed up to 7300MB/s Write Speed Up to 6900 MB/s ( New G series)',
+	'High-end PCIe 4.0 NVMe SSD designed for the highest level of gaming performance and stability
+Stable high speed performance read/write speeds up to 7300(Read)/6900(Write)MB/s (2TB model) powerd by Nextorage and Phison technologies.
+Tested by proffesional esports player to provide the best gaming environment
+5-year limited warranty of peace of mind
+Downloadable data cloning software ( ACRONIS True Image OEM)',
 	'category8',
 	157),
+
 	('STG-009',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Nextorage Japan Internal SSD 4TB for PS5 and PC Storage Expansion M.2 2280 with Heatsink PCIe Gen4.0*4 NVMe 3D TLC NAND NEM-PA4TB/N SYM 3000TBW with Maximum Transfer Rate Read 7300MB/s Write6900MB/s',
+	'Expand your PS5 storage for more game contents storage.
+Ultra-fast PCIe Gen.4.0x4 realized max read 7300MB/s* max write 6900MB/s* . *Tested in Nextorage PC environment.
+Optimized cooling structure for stable game play. An aluminum heatsink, coated with a highly thermally radioactive black alumite.
+Include easy installation instructions for PS5.
+5-Year Limited Warranty for peace of mind',
 	'category8',
 	158),
+
 	('STG-010',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Nextorage Japan Internal SSD 2TB for PS5 and PC Storage Expansion M.2 2280 with Heatsink PCIe Gen4.0 NVMe 3D TLC NAND NEM-PA2TB/N SYM 1400TBW with Maximum Transfer Rate Read: 7300MB/s, Write: 6900MB/s',
+	'Expand your PS5 storage for more game contents storage.
+Ultra-fast PCIe Gen.4.0x4 realized max read 7300MB/s* max write 6900MB/s* . *Tested in Nextorage PC environment.
+Optimized cooling structure for stable game play. An aluminum heatsink, coated with a highly thermally radioactive black alumite.
+Include easy installation instructions for PS5.
+5-Year Limited Warranty for peace of mind',
 	'category8',
 	159),
+
 	('STG-011',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'SAMSUNG 990 PRO M.2 2280 2TB PCI-Express Gen 4.0 x4, NVMe 2.0 V7 V-NAND 3bit MLC Internal Solid State Drive (SSD) MZ-V9P2T0B/AM',
+	'The flagship 990 PRO and 990 PRO with heatsink is a premium NVMe Gen 4 internal SSD providing the best gaming experience with ultimate power efficiency and thermal control solutions.
+Optimized for real-world and gaming workloads with up to 65% improvement in random performance to the 980 PRO (1,400k/1,550k for Random Read/Write)
+Reaching the PCIe Gen4 performance limits showing up to 7,450/6,900MB/s for Sequential Read/Write
+Power efficiency improved up to 50% than the 980 PRO thanks to the new 8nm controller with low power architecture
+Heatsink model maximizes the cooling effect and prevents performance degradation due to overheating
+Optimized to support DirectStorage API technology showing faster game loading and playing times
+Slim and futuristic designed heatsink with RGB lights.',
 	'category8',
 	160),
+
 	('STG-012',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'ADATA PREMIUM SSD FOR PS5 M.2 2280 2TB PCI-Express 4.0 x4, NVMe 1.4 3D NAND Internal Solid State Drive (SSD) APSFG-2T-CSUS',
+	'M.2 2280
+	2TB
+	PCI-Express 4.0 x4, NVMe 1.4',
 	'category8',
 	161),
+
 	('STG-013',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'WD_BLACK SN850X NVMe M.2 2280 1TB PCI-Express 4.0 x4 Internal Solid State Drive (SSD) WDS100T2X0E',
+	'Get the ultimate gaming edge over your competition with insane speeds up to 7,300 MB/s for top-level performance and ridiculously short load times.
+Soup up your gaming experience with extremely low latency that loads graphics fast with minimal stutter and lagging for smooth, satisfying action.
+A range of capacities from 1TB to 4TB means you get to keep more of today’s games that can take up 200GB or more of storage.
+The 1TB and 2TB drives have an optional version with RGB lighting plus heatsink to help maintain peak performance through the most intense gaming sessions.
+Game Mode 2.0 serves up even more PC performance-boosting features like load prediction to ready game assets for fast in-game loading.
+Output approaching the limits of the PCIe® Gen4 interface helps you get ultra-fast performance out of your storage and system components.',
 	'category8',
 	162),
+
 	('STG-014',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Solidigm P41 Plus 2TB M.2 2280 PCIe 4.0 NVMe Gen4 Internal Solid State Drive (SSD) SSDPFKNU020TZX1',
+	'The Solidigm P41 Plus Solid-State Drive (SSD) delivers excellent PCIe Generation 4 storage performance and value for your personal computer (PC)
+With up to 4,125 MB/s sequential read speeds the P41 Plus delivers storage performance that enables your everyday productivity and innovation
+M.2 form factor, NVMe version 1.4 SSD available in 512GB, 1TB and 2TB capacities.
+Excellent Performance for Everyday Work and Play the P41 Plus is durable and reliable with outstanding power savings and heat efficiency
+Pair the P41 Plus SSD with free and downloadable Solidigm Synergy software for added performance and faster access to your files and programs
+Solidigm was founded in 2021 after Intel sold its SSD and NAND business to SK hynix. Backed by decades of SSD innovation and market leadership, Solidigm SSDs deliver world-class performance and reliability you can trust.',
 	'category8',
 	163),
+
 	('STG-015',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'SAMSUNG 990 PRO M.2 2280 1TB PCIe Gen 4.0 x4, NVMe 2.0 V7 V-NAND 3bit MLC Internal Solid State Drive (SSD) MZ-V9P1T0B/AM',
+	'The flagship 990 PRO and 990 PRO with heatsink is a premium NVMe Gen 4 internal SSD providing the best gaming experience with ultimate power efficiency and thermal control solutions.
+Optimized for real-world and gaming workloads with up to 65% improvement in random performance to the 980 PRO (1,400k/1,550k for Random Read/Write)
+Reaching the PCIe Gen4 performance limits showing up to 7,450/6,900MB/s for Sequential Read/Write
+Power efficiency improved up to 50% than the 980 PRO thanks to the new 8nm controller with low power architecture
+Heatsink model maximizes the cooling effect and prevents performance degradation due to overheating
+Optimized to support DirectStorage API technology showing faster game loading and playing times
+Slim and futuristic designed heatsink with RGB lights.',
 	'category8',
 	164),
+
 	('STG-016',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Seagate IronWolf Pro 20TB NAS Hard Drive 7200 RPM 256MB Cache CMR SATA 6.0Gb/s 3.5" Internal HDD NE-ST20000NE000',
+	'7200 RPM 256MB Cache
+SATA 6.0Gb/s
+Enterprise NAS Hard Drives',
 	'category8',
 	165),
+
 	('STG-017',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
-	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
+	'Seagate BarraCuda ST4000DM004 4TB 5400 RPM 256MB Cache SATA 6.0Gb/s 3.5" Hard Drives Bare Drive - OEM',
+	'Cost-effective storage upgrade for laptop or desktop computers
+Store all your games, music, movies and more with up to 4TB of storage
+SATA 6Gb/s interface optimizes burst performance; 256MB Cache',
 	'category8',
 	166),
+
 	('STG-018',
 	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
 	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
 	'category8',
 	167),
 	('STG-019',
-	'Western Digital WD Green SN350 NVMe M.2 2280 2TB PCI-Express 3.0 x4 Internal Solid State Drive (SSD) WDS200T3G0C',
+	'Synology HAT3300 8TB Plus Series SATA HDD 3.5',
 	'A Quick Upgrade, Monitor Your Drive s Health, More Room For What Is Important, Be Confident In Your Choice',
 	'category8',
 	168),
@@ -169,7 +244,10 @@ INSERT INTO products (product_id, pname, description, category_id, qty_in_store)
 
 INSERT INTO configurations(product_id, config_name, config_price) VALUES
 	('STG-001','SN350 NVMe M.2 2280 1TB',45.99),
-	('STG-002','SN350 NVMe M.2 2280 1TB',45.99),
+	('STG-002','2TB',85.00),
+	('STG-002','960GB',38.99),
+	('STG-002','480GB',26.99),
+	('STG-002','240GB',24.27),
 	('STG-003','SN350 NVMe M.2 2280 1TB',45.99),
 	('STG-004','SN350 NVMe M.2 2280 1TB',45.99),
 	('STG-005','SN350 NVMe M.2 2280 1TB',45.99),
