d22c3dfd309c89934922c14f2dff6bf1f48ccf7b
Dinh Nguyen Cong Quy <congquyk66hust@gmail.com>
2023-07-06 22:51:17 +0700

    Add all product images

---
+++ database/insert_operating_system.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (category_id, category_name) VALUES 
+
+INSERT INTO products (product_id, pname, description, category_id, qty_in_store) VALUES
+
+INSERT INTO configurations(product_id, config_name, config_price) VALUES
