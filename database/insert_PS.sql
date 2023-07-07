4b6d2d6d2cd4dbc67be5f4addd87cef1805e574e
bmquang-20214925 <herewego1812@gmail.com>
2023-07-07 16:26:06 +0700

    commit sql

---
+++ database/insert_PS.sql
@@ -0,0 +1,246 @@
+INSERT INTO categories (category_id, category_name) VALUES 
+    ('category1', 'CPU'),
+    ('category2', 'RAM'),
+    ('category3', 'CPU Cooler'),
+    ('category4', 'Motherboard'),
+    ('category5', 'Video Card'),
+    ('category6', 'Case'),
+    ('category7', 'Power Supplies'),
+    ('category8', 'Storages'),
+    ('category9', 'Operating System'),
+    ('category10', 'Accessories');
+
+INSERT INTO products (product_id, pname, description, category_id, qty_in_store) VALUES
+('PS-001', 'CORSAIR RM850e Fully Modular Low-Noise ATX Power Supply - ATX 3.0 & PCIe 5.0 Compliant - 105°C-Rated Capacitors - 80 PLUS Gold Efficiency - Modern Standby Support', 
+    'ATX
+Full Modular
+80 PLUS GOLD Certified
+100 - 240 V 47 - 63 Hz
++3.3V@20A, +5V@20A, +12V@70.8A, +5VSB@3A', 'category7', 999),
+
+("PS-002", 'CORSAIR RMx Series (2021) RM750x CP-9020199-NA 750 W ATX12V / EPS12V 80 PLUS GOLD Certified Full Modular Power Supply',
+'Fully modular, so you only connect the cables that your system needs.
+A 135mm ML fan utilizes a magnetic levitation bearing and custom engineered rotors for high performance, low noise, and superior reliability.
+EPS12V connector for compatibility with modern graphics cards and motherboards.
+100% Industrial-grade, 105°C rated Japanese capacitors ensure unwavering power and reliability.
+Supports Modern Standby sleep mode for extremely fast wake-from-sleep times and better low-load efficiency.
+Zero RPM fan mode for near-silent operation at low to medium loads.
+Resonant LLC topology with DC-to-DC conversion provides clean, consistent power, reduces coil whine for quieter operation, and enables use of more energy efficient sleep states.
+Ten-year warranty for complete peace of mind.','category7',999),
+("PS-003", 'Segotep 750W Power Supply Full Modular 80 Plus Gold PSU, 600W 12VHPWR Cable Included, 12+4PIN port and Dual 6+2Pin ports for Different Graphics cards, Silent Fan mode, ATX 3.0 Gaming Power Supply',
+'[ATX 3.0 Ready] Segotep power supply comes with 12+4PIN PCIe 5.0 port and dual Regular 6+2PIN PCIe ports for different Nvidia 10 series, 20 series, 30 series GPUs and AMD GPUs. More Stable and Safe than normal ATX power supply.
+[PCIe 5.0 Ready] The PSU can hold up to 200% total power excursion & 300% GPU power excursion.
+[80 Plus Gold Certified] The 80 PLUS Gold certification promises higher efficiency & lower energy consumption.
+[Fully Modular] Full module with flat black cables, improve airflow & strengthen cable management.
+[Stable +12V Output] DC-DC structure, Full Bridge LLC resonance assist, 12V synchronous rectification provide stable & safe output.','category7',999),
+   
+("PS-004", 'CORSAIR HX850 Fully Modular Ultra-Low Noise ATX Power Supply - Triple EPS12V Connectors - Fluid Dynamic Bearing Fan - Zero RPM Fan Mode - 105°C Japanese Capacitors - 80 PLUS Platinum Efficiency',
+'Fully modular cabling, so you only have to connect the cables that your system needs.
+Triple EPS12V connectors for full compatibility with modern graphics cards and motherboards.
+A 140mm fluid dynamic bearing fan with a specially calculated fan curve ensures minimum fan noise and efficient, effective cooling, even at full load.
+Zero RPM fan mode for near-silent operation at low to medium loads.
+100% Industrial-grade, 105°C rated Japanese capacitors ensure unwavering power and reliability.
+80 PLUS Platinum certified for up to 92% efficiency, for exceptionally low power consumption, less noise, and cooler temperatures.
+Supports Modern Standby sleep mode for extremely fast wake-from-sleep times and better low-load efficiency.
+Fully digital platform resonant LLC topology with DC-to-DC conversion provides clean, consistent power, reduces coil whine for quieter operation, and enables
+use of more energy efficient sleep states.','category7',999),
+   
+("PS-005", 'CORSAIR RMx Series (2021) RM1000x CP-9020201-NA 1000 W ATX12V / EPS12V SLI Ready CrossFire Ready 80 PLUS GOLD Certified Full Modular Active PFC Power Supply',
+'Fully modular, so you only connect the cables that your system needs.
+A 135mm ML fan utilizes a magnetic levitation bearing and custom engineered rotors for high performance, low noise, and superior reliability.
+EPS12V connector for compatibility with modern graphics cards and motherboards.
+100% Industrial-grade, 105°C rated Japanese capacitors ensure unwavering power and reliability.
+Supports Modern Standby sleep mode for extremely fast wake-from-sleep times and better low-load efficiency.
+Zero RPM fan mode for near-silent operation at low to medium loads.
+Resonant LLC topology with DC-to-DC conversion provides clean, consistent power, reduces coil whine for quieter operation, and enables use of more energy efficient sleep states.
+Ten-year warranty for complete peace of mind.','category7',999),
+   
+("PS-006", 'Segotep 850W ATX 3.0 Power Supply, Full Modular 80 Plus Gold, PCIe 5.0 12+4PIN port and Dual 6+2Pin ports, Suitable for ALL Graphics Cards, Native 600W 12VHPWR Cable, Silent Fan mode',
+'[ATX 3.0 & PCIe 5.0 Ready] The PSU can hold up to 200% total power excursion & 300% GPU power excursion. This power supply comes with 12+4PIN PCIe 5.0 port and dual Regular 6+2PIN PCIe ports for ALL Graphics cards.
+[80 Plus Gold Certified] The 80 PLUS Gold certification promises higher efficiency & lower energy consumption.
+[Fully Modular] Full module with flat black cables, improve airflow & strengthen cable management.
+[Native 12VHPWR Cable] It can generate up to 600W of power to PCIe 5.0 graphics cards.
+[Stable +12V Output] DC-DC structure, Full Bridge LLC resonance assist, 12V synchronous rectification provide stable & safe output.','category7',999),
+   
+("PS-007", 'Rosewill CMG1000G5 PCIE 5.0, 80 GOLD Full Modular Gaming Power Supply, 12VHPWR Cable, 4080 4090 ATX 3.0 Compatible, 1000W PSU',
+'PCIe 5.0 and ATX 3.0 Compatible
+Full Modular
+80 PLUS GOLD Certified
++3.3V@25A, +5V@25A, +12V@83.3A, -12V@0.3A, +5VSB@3A','category7',999),
+   
+("PS-008", 'Thermaltake Smart Series 500W SLI/CrossFire Ready Continuous Power ATX 12V V2.3 / EPS 12V 80 PLUS Certified Active PFC Power Supply Haswell Ready PS-SPD-0500NPCWUS-W',
+'Intel ATX 12V 2.31 & EPS 12V 2.92
+Hardlined
+80 PLUS Certified
+100 - 240 V 50/60 Hz
++3.3V@20A, +5V@15A, +12V@35A, -12V@0.5A, +5VSB@2.5A','category7',999),
+   
+("PS-009", 'Segotep 850W White PCIe 5.0 Full Modular 80 Plus Gold PSU ATX 3.0 Gaming Power Supply, 12VHPWR Cable, 12+4PIN port and Dual 6+2Pin ports for Different Graphics Cards, Silent Fan mode',
+'[ATX 3.0 & PCIe 5.0 Ready] The PSU can hold up to 200% total power excursion & 300% GPU power excursion. This power supply comes with 12+4PIN PCIe 5.0 port and dual Regular 6+2PIN PCIe ports for different Graphics cards.
+[80 Plus Gold Certified] The 80 PLUS Gold certification promises higher efficiency & lower energy consumption.
+[Fully Modular] Full module with flat white cables, improve airflow & strengthen cable management.
+[Native 12VHPWR Cable] It can generate up to 600W of power to PCIe 5.0 graphics cards.
+[Stable +12V Output] DC-DC structure, Full Bridge LLC resonance assist, 12V synchronous rectification provide stable & safe output.','category7',999),
+   
+("PS-010", 'Super Flower Leadex V Platinum PRO 1000W ATX 80 PLUS PLATINUM Certified Power Supply, Smallest 130mm 1000W ATX PSU, 10 Years Warranty, Patent Super Connectors, Full Modular, SF-1000F14TP',
+'Smallest ATX 1000W Platinum Power Supply (Length 130mm)
+Full modular design with all ribbon cables to maximize flexibility
+80 PLUS Platinum Certified,90% 92% 89% Efficiency At 20% 50% 100% Rated Load.
+Dual overpower protection
+120mm Ultra Silent Fluid Dynamic Bearing Fan','category7',999),
+   
+("PS-011", 'CORSAIR RM1000e Fully Modular Low-Noise ATX Power Supply - ATX 3.0 & PCIe 5.0 Compliant - 105°C-Rated Capacitors - 80 PLUS Gold Efficiency - Modern Standby Support',
+'ATX
+Full Modular
+80 PLUS GOLD Certified
+100 - 240 V 47 - 63 Hz
++3.3V@20A, +5V@20A, +12V@83.3A, +5VSB@3A','category7',999),
+   
+("PS-012", 'ASUS TUF Gaming 850W Gold (850 Watt, ATX 3.0 Compatible, Fully Modular Power Supply, 80+ Gold, Military-grade Components, Dual Ball Bearing, Axial-tech Fan, PCB Coating)',
+'Capacitors and chokes pass demanding tests to achieve Military-grade Certification.
+Dual ball fan bearings can last up to twice as long as sleeve bearing designs.
+A protective PCB coating protects against moisture, dust, and extreme temperatures.
+An 80 Plus Gold Certification is the result of Japanese capacitors and other premium components.
+ATX 3.0 Compatible: TUF Gaming Gold is compliant with ATX 3.0 guidelines and bundled with a 16-pin PCIe cable that can pipe up to 600W of power to PCIe Gen 5.0 graphics cards.
+Axial-tech fan design features a smaller fan hub that facilitates longer blades and a barrier ring that increases downward air pressure.
+Fully modular etched cables keep your rig neat and tidy.','category7',999),
+   
+("PS-013", 'CORSAIR RMx Shift Series RM1200x Shift Fully Modular 80PLUS Gold ATX Power Supply',
+'Innovative Easy-Access Connection Position.
+Fully Modular Micro-Fit PSU Connectors.
+140mm Fluid Dynamic Bearing Fan.
+Intel® ATX 3.0 Certified: Compliant with the ATX 3.0 power standard, supporting the PCIe Gen 5 platform and resisting transient power spikes.
+100% 105°C-Rated Japanese Electrolytic Capacitors.
+Modern Standby Compatible.
+80 PLUS Gold Certified.
+Zero RPM Fan Mode Support.
+Resonant LLC Topology with DC-to-DC Conversion.','category7',999),
+   
+("PS-014", 'CORSAIR RM Series RM750 750 W ATX 80 PLUS GOLD Certified Full Modular Power Supply',
+'ATX
+Full Modular
+80 PLUS GOLD Certified','category7',999),
+   
+("PS-015", 'GPNE 1600W ATX Power Supply, Fully Modular PSU for Gaming PC Case, 6 x 6+2 Pin PCIE ports, Lower Noise 140mm Double Ball Bearing Fan',
+'','category7',999),
+   
+("PS-016", 'EVGA 700 BR 100-BR-0700-K1 700 W ATX12V / EPS12V SLI CrossFire 80 PLUS BRONZE Certified Non-Modular Power Supply',
+'80 PLUS Bronze certified, with 85% efficiency or higher under typical loads
+Single 12V. Rail
+Active Power Factor Correction (PFC)
+Heavy-duty protections, including OVP (Over Voltage Protection), UVP (Under Voltage Protection), OCP (Over Current Protection), OPP (Over Power Protection), SCP (Short Circuit Protection), and OTP (Over Temperature Protection)
+3 Year Warranty','category7',999),
+   
+("PS-017", 'Rosewill HIVE Series, HIVE-850S, 850W Fully Modular Power Supply, 80 PLUS BRONZE Certified, Single +12V Rail, SLI & CrossFire Ready, Black',
+'850 Watt Power Supply
+ATX 12V v2.31 / EPS 12V v2.92
+100 - 240 V 50 / 60 Hz
+80 PLUS BRONZE Certified
+Full Modular Design PSU
+SLI & CrossFire Ready
+Silent 135mm Fan
+Strong Single +12V Rail','category7',999),
+   
+("PS-018", 'SILVERSTONE SFX Series 80 Plus Bronze Certificated SST-ST45SF-V3 450W SFX Active PFC Power Supply',
+'80+ Bronze Certificated
+SFX Active PFC
++3.3V@20A, +5V@20A, +12V@37.5A, +5VSB@3A, -12V@0.3A
+Supports standard SFX form factor and ATX via included bracket','category7',999),
+   
+("PS-019", 'Enermax MarbleBron RGB White 850W 80 PLUS BRONZE Certified, Semi-Modular, ATX12V / EPS12V, 5 Year Warranty, Active PFC Power Supply – EMB850EWT-W-RGB',
+'80 PLUS Bronze certified
+Semi-modular design with black flat cables
+Japanese electrolytic capacitors
+Compact chassis with 140mm chassis in-depth
+120mm high pressure fan with Smart Airflow
+Support RGB motherboard software sync
+Luminous RGB fan and side panel
+RGB Function with built-in 14 smart lighting modes
+Multiple circuitry protection of OVP, UVP, OPP, SCP and OTP','category7',999),
+   
+("PS-020", 'Super Flower Leadex V Platinum PRO 1000W ATX 80 PLUS PLATINUM Certified Power Supply, Smallest 130mm 1000W ATX PSU, 10 Years Warranty, Patent Super Connectors, Full Modular, SF-1000F14TP',
+'Smallest ATX 1000W Platinum Power Supply (Length 130mm)
+Full modular design with all ribbon cables to maximize flexibility
+80 PLUS Platinum Certified,90% 92% 89% Efficiency At 20% 50% 100% Rated Load.
+Dual overpower protection
+120mm Ultra Silent Fluid Dynamic Bearing Fan','category7',999)
+   
+
+   
+;
+
+
+
+INSERT INTO configurations(product_id, config_name, config_price) VALUES
+    ('PS-001','850 W', 119.99),
+    ('PS-001','1000 W', 159.99),
+    ('PS-001','750 W', 99.99),
+
+    ('PS-002','1200 W', 269.99),
+    ('PS-002','1000 W', 209.99),
+    ('PS-002','850 W', 159.99),
+    ('PS-002','750 W', 119.99),
+
+    ('PS-003','750 W', 89.99),
+    ('PS-003','850 W', 109.99),
+
+    ('PS-004','850 W', 134.99),
+
+    ('PS-005','1200 W', 269.99),
+    ('PS-005','1000 W', 189.99),
+    ('PS-005','850 W', 159.99),
+
+    ('PS-006','850 W', 109.99),
+    ('PS-006','750 W', 89.99),
+
+    ('PS-007','1200 W', 169.99),
+    ('PS-007','1000 W', 159.99),
+
+    ('PS-008','750 W', 69.99),
+    ('PS-008','600 W', 67.99),
+    ('PS-008','550 W', 117.99),
+
+    ('PS-009','850 W', 139.99),
+    ('PS-009','750 W', 89.99),
+
+    ('PS-010','1000 W', 299.99),
+    ('PS-010','850 W', 249.99),
+
+    ('PS-011','1000 W', 159.99),
+    ('PS-011','850 W', 119.99),
+    ('PS-011','750 W', 99.99),
+
+    ('PS-012','1000 W', 169.99),
+    ('PS-012','850 W', 129.99),
+    ('PS-012','750 W', 123.99),
+
+    ('PS-013','1200 W', 269.99),
+    ('PS-013','1000 W', 209.99),
+    ('PS-013','850 W', 159.99),
+
+
+    ('PS-014','750 W', 116.79),
+
+    ('PS-015','1600 W', 139.99),
+
+    ('PS-016','700 W', 79.99),
+    ('PS-016','600 W', 96.48),
+    ('PS-016','500 W', 68.44),
+
+    ('PS-017','850 W', 99.99),
+    ('PS-017','650 W', 69.99),
+
+    ('PS-018','450 W', 81.99),
+
+    ('PS-019','850 W', 99.99),
+
+    ('PS-020','1000 W', 299.99),
+    ('PS-020','850 W', 249.99)
+    
+;
+
+
+
+
+
+
